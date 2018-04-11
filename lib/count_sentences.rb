af6ca46ae6732069b46a33b82b1d812d10830bb4
Sean Febo <seanfebo@techie.com>
2018-04-11 16:36:28 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -29,10 +29,5 @@ class String
   def count_sentences
   
    self.split(/[.?!]/).reject{|x| x.empty?}.count
-   if new == 0 
-     return 0 
-    else 
-      return new.count 
-    end
   end
 end
